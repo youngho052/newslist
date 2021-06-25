e81a6380a47f2693b36c75fc788dbfecdec9d1c5
youngho052 <qkrdudgh2701@gmail.com>
2021-06-25 20:39:17 +0900

    list초기화면

--- src/Pages/NewsList/NewsList.jsx
+++ src/Pages/NewsList/NewsList.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import axios from "axios";
+import { API } from "../../Config";
 
-function NewsList(props) {
-  return <div></div>;
+function NewsList() {
+  const [newsList, setNewsList] = useState();
+
+  useEffect(() => {
+    axios.get(API).then((res) => setNewsList(res?.data.response.docs));
+  }, []);
+
+  console.log(newsList);
+  return (
+    <div>
+      {newsList?.map((item, index) => {
+        return (
+          <a href={item.web_url}>
+            <span style={{ display: "block" }}>
+              {/* {item.abstract.length > 30
+              ? item.abstract.slice(0, 30)
+              : item.abstract} */}
+              {item.abstract}
+            </span>
+          </a>
+        );
+      })}
+    </div>
+  );
 }
 
 export default NewsList;
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NewsList from "./Pages/NewsList/NewsList";
 
 function Routes() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={} />
+        <Route exact path="/" component={NewsList} />
       </Switch>
     </Router>
   );
